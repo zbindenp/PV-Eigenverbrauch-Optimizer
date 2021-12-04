c2a03134527dc601f24d7bcd8b3ef97caf8e1593
Silvan Schaer <silvan.schaer@bison-group.com>
2021-12-04 10:48:26 +0100

    add badge to tip

--- pvoptimizer/src/app/app.module.ts
+++ pvoptimizer/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HourComponent } from './hour/hour.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,9 +22,11 @@ import { HoursToTimePipe } from './hours-to-time.pipe';
     BrowserAnimationsModule,
     MatCardModule,
     MatProgressBarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
